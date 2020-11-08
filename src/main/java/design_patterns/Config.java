6d79ba6079ff0c5847efda3bb76fce0c97d1ca66
BlumaSavrasov <BlumaSavrasov@users.noreply.github.com>
2020-11-08 13:10:20 +0200

    rewrite distribution service to spring

--- src/main/java/design_patterns/Config.java
+++ src/main/java/design_patterns/Config.java
@@ -1,9 +1,21 @@
 package design_patterns;
 
+import lombok.SneakyThrows;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 public class Config {
+        @SneakyThrows
+        public static void main(String[] args) {
+            AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+            DistributionService distributionService = context.getBean(DistributionService.class);
+            while (true){
+                distributionService.sendMail();
+                Thread.sleep(500);
+            }
+        }
 }
+
---
+++ src/main/java/real_spring/how_to_refresh_prototype_in_singleton/ColorFrame.java
@@ -0,0 +1,7 @@
+package real_spring.how_to_refresh_prototype_in_singleton;
+
+import javax.swing.*;
+
+public class ColorFrame extends JFrame {
+
+}
